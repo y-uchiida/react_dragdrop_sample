5aac949bac0c9bb0132c761aab1fc8d46d31dcd6
yugo uchiida <y.uchiida@nirneege.com>
2022-12-08 10:25:33 +0900

    implement drag preview image customize

--- src/components/react-dnd/Chess/Knight.tsx
+++ src/components/react-dnd/Chess/Knight.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useDrag } from "react-dnd";
+import { DragPreviewImage, useDrag } from 'react-dnd';
 import { ItemTypes } from "./ItemTypes";
+import { knightImage } from "./KnightImage";
 
 const knightStyle: React.CSSProperties = {
   fontSize: 60,
@@ -16,7 +17,9 @@ const Knight = () => {
   // useDrag の返り値の配列の第一要素には、collect() 関数が返すオブジェクトが入る
   // 2つ目の要素は、connector() 関数で、これによってドラッグ中のオブジェクト(Item) とDOM の要素が関連づけされる
   // ドラッグするJSXノードのref プロパティに、drag を渡す
-  const [{ isDragging }, drag] = useDrag(() => (
+  // 返り値配列の3つ目の要素は、ドラッグ中のプレビュー表示とDOMを関連づけするconnector() 関数
+  // ドラッグ中画像を表現するためのコンポーネント DragPreviewImage に、返り値のconnector() と表示したい画像パスを渡す
+  const [{ isDragging }, drag, preview] = useDrag(() => (
     {
       // typeは、item オブジェクトを識別するプロパティ
       type: ItemTypes.KNIGHT,
@@ -29,15 +32,19 @@ const Knight = () => {
   ));
 
   return (
-    <div
-      style={{
-        ...knightStyle,
-        opacity: isDragging ? 0.5 : 1, // ドラッグ中は半透明にする
-      }}
-      ref={drag} // useDrag() hooks から取り出したdragを渡す
-    >
-      ♘
-    </div>
+    <>
+      {/* DragPreviewImage に、Knight コンポーネントがドラッグ中に表示される画像を設定する */}
+      <DragPreviewImage connect={preview} src={knightImage} />
+      <div
+        style={{
+          ...knightStyle,
+          opacity: isDragging ? 0.5 : 1, // ドラッグ中は半透明にする
+        }}
+        ref={drag} // useDrag() hooks から取り出したdragを渡す
+      >
+        ♘
+      </div>
+    </>
   );
 };
 
