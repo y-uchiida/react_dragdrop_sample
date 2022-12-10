41df20481717b50f7f6ffaf0862cb315d743b6b0
yugo uchiida <y.uchiida@nirneege.com>
2022-12-11 00:52:13 +0900

    add drag item info box

--- src/components/dnd-kit/kanban-board-sample/KanbanBoard.tsx
+++ src/components/dnd-kit/kanban-board-sample/KanbanBoard.tsx
@@ -1,9 +1,13 @@
-import { DndContext, rectIntersection } from "@dnd-kit/core";
+import { DndContext, rectIntersection, UniqueIdentifier } from "@dnd-kit/core";
 import { KanbanLane } from "./KanbanLane";
 import { AddCard } from "./AddCard";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { useState } from "react";
 import { Card } from "./types";
+
+const infoBoxStyle: React.CSSProperties = {
+	width: 'max-content',
+}
 
 export const KanbanBoard = () => {
 	const [todoItems, setTodoItems] = useState<Array<Card>>([]);
@@ -16,18 +20,34 @@ export const KanbanBoard = () => {
 		setuItems([...uItems, { title }]);
 	};
 
+	const [droppedContainer, setDroppedContainer] = useState<UniqueIdentifier | undefined>();
+	const [dragItemTitle, setDragItemTitle] = useState<string | undefined>();
+	const [dragItemIndex, setDragItemIndex] = useState<number | undefined>();
+	const [dragItemParent, setDragItemParent] = useState<string | undefined>();
+
 	return (
 		<DndContext
 			collisionDetection={rectIntersection}
+			onDragMove={(e) => {
+				setDroppedContainer(e.over?.id);
+				setDragItemTitle(e.active.data.current?.title);
+				setDragItemIndex(e.active.data.current?.index);
+				setDragItemParent(e.active.data.current?.parent);
+			}}
+
 			onDragEnd={(e) => {
 				const container = e.over?.id;
 				const title = e.active.data.current?.title ?? "";
 				const index = e.active.data.current?.index ?? 0;
 				const parent = e.active.data.current?.parent ?? "ToDo";
+
+
 				if (container === "ToDo") {
 					setTodoItems([...todoItems, { title }]);
 				} else if (container === "Done") {
 					setDoneItems([...doneItems, { title }]);
+				} else if (container === "In Progress") {
+					setInProgressItems([...uItems, { title }]);
 				} else if (container === "Unassigned") {
 					setuItems([...uItems, { title }]);
 				} else {
@@ -43,13 +63,10 @@ export const KanbanBoard = () => {
 						...doneItems.slice(0, index),
 						...doneItems.slice(index + 1),
 					]);
+				} else if (parent === "In Progress") {
+					setInProgressItems([...uItems.slice(0, index), ...uItems.slice(index + 1)]);
 				} else if (parent === "Unassigned") {
 					setuItems([...uItems.slice(0, index), ...uItems.slice(index + 1)]);
-				} else {
-					setInProgressItems([
-						...inProgressItems.slice(0, index),
-						...inProgressItems.slice(index + 1),
-					]);
 				}
 			}}
 		>
@@ -62,6 +79,12 @@ export const KanbanBoard = () => {
 					<KanbanLane title="Unassigned" items={uItems} />
 				</Flex>
 			</Flex>
+			<Box padding={5} fontSize={20} style={infoBoxStyle}>
+				<p>droppedContainer: {droppedContainer?.toString()}</p>
+				<p>dragItemTitle: {dragItemTitle?.toString()}</p>
+				<p>dragItemIndex: {dragItemIndex?.toString()}</p>
+				<p>dragItemParent: {dragItemParent?.toString()}</p>
+			</Box>
 		</DndContext>
 	);
 }
