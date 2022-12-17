adbab7555945922fcbe8f7a4565a0efdf7713a20
yugo uchiida <y.uchiida@nirneege.com>
2022-12-17 11:43:35 +0900

    implement Dnd information box in schedule app

--- src/components/dnd-kit/schedule-sample/DndSchedule.tsx
+++ src/components/dnd-kit/schedule-sample/DndSchedule.tsx
@@ -1,11 +1,11 @@
 import { Spacer } from './atoms/Spacer'
-import { DndContext, DragOverlay } from '@dnd-kit/core';
+import { DndContext, DragOverlay, UniqueIdentifier } from '@dnd-kit/core';
 import {
 	restrictToVerticalAxis,
 	createSnapModifier,
 	restrictToWindowEdges,
 } from '@dnd-kit/modifiers';
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import { ScheduleGridLane } from './ScheduleGridLane';
 import { ScheduleGutter } from './ScheduleGutter';
 import { ScheduleHeader } from './ScheduleHeader';
@@ -16,7 +16,7 @@ const snapToGridModifier = createSnapModifier(gridSize);
 const scheduleWrapperStyle: CSSProperties = {
 	background: '#333',
 	width: '100%',
-	height: 'calc(100vh - 80px)',
+	height: 'calc(100vh - 280px)',
 }
 
 const scheduleGridStyle: CSSProperties = {
@@ -25,14 +25,46 @@ const scheduleGridStyle: CSSProperties = {
 	flexDirection: 'row',
 	width: '100%',
 	overflowY: 'scroll',
-	height: 'calc(100vh - 120px)'
+	height: 'calc(100vh - 321px)'
+}
+
+const dndInfoBoxStyle = {
+	height: 184,
+	marginBottom: 16,
+	background: '#444',
+	border: '1px solid #999',
+	padding: 8
 }
 
 export const DndSchedule = () => {
+
+	const [droppedContainer, setDroppedContainer] = useState<UniqueIdentifier | undefined>();
+	const [dragItemId, setDragItemId] = useState<string | undefined>();
+	const [dragItemTitle, setDragItemTitle] = useState<string | undefined>();
+	const [dragItemParent, setDragItemParent] = useState<string | undefined>();
+
 	return (
 		<DndContext
 			modifiers={[snapToGridModifier]}
+
+			/* ドラッグのたびに発火するするイベントのハンドラ */
+			onDragMove={(e) => {
+				setDroppedContainer(e.over?.id);
+				setDragItemId(e.active.data.current?.uid);
+				setDragItemTitle(e.active.data.current?.title);
+				setDragItemParent(e.active.data.current?.parent);
+			}}
+
+
+			/* ドラッグ終了時のイベントハンドラ */
+			onDragEnd={(e) => { }}
 		>
+			<div style={dndInfoBoxStyle}>
+				<p>dragItemId: {dragItemId?.toString()}</p>
+				<p>droppedContainer: {droppedContainer?.toString()}</p>
+				<p>dragItemTitle: {dragItemTitle?.toString()}</p>
+				<p>dragItemParent: {dragItemParent?.toString()}</p>
+			</div>
 			<div style={scheduleWrapperStyle}>
 				<ScheduleHeader />
 				<div style={scheduleGridStyle}>
